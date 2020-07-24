d5f62dc6077be72f5d2ba4a8f9a49a13d411f1e0
TonatiujSanchez <tonsanzjimz@gmail.com>
2020-07-24 03:59:03 -0500

    Juego completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcadogame';
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+  palabra = 'ANGULAR';
+  palabraOculta = '';
+  intentos = 0;
+
+  gano = false;
+  perdio = false;
+  constructor(){
+    this.palabraOculta = '_ '.repeat( this.palabra.length );
+  }
+
+  comprobar( letra:string ){
+    this.existeLetra( letra );
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      } 
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
+  }
+  
+  verificaGane(){
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+    
+    if( palabraEvaluar === this.palabra ){
+      this.gano = true;
+      console.info('Ganaste!!');
+    }
+    
+    if (this.intentos >= 9) {
+      this.perdio = true;
+      console.error('Ups! Ya perdiste');
+    }    
+
+  }
+
+  existeLetra( letra ){
+    if ( this.palabra.indexOf( letra ) >= 0 ) {
+      console.log('Si exsite');
+    }else{
+      console.log('no existe');
+      this.intentos ++;
+    }
+  }
+  
 }
